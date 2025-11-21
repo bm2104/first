f76567201fd60a723b0ae91306c9280a99b95181
bm2104 <borvx2@gmail.com>
2025-11-22 08:53:32 +0900

    Add files via upload

---
+++ 보고서_자동화.py
@@ -0,0 +1,175 @@
+import pandas as pd
+from datetime import datetime, timedelta
+import streamlit as st
+from 보고서_함수 import calculate_summary, process_data, week_buy_price, data_process 
+from io import BytesIO
+
+# 날짜 확인 및 지정
+today = datetime.today()
+to_day = today.strftime('%Y%m%d')
+days = today.strftime('%m월 %d일')
+days_from_wed = (today.weekday() - 2) % 7 + 7
+days_from_fri = (today.weekday() - 4) % 7 
+days_from_yester = (today.weekday() - 1) % 7
+toDay = (today - timedelta(days=days_from_yester)).strftime('%Y/%m/%d')
+td = (today - timedelta(days=days_from_yester)).strftime('%Y-%m-%d')
+lastweek = (today - timedelta(days=days_from_wed)).strftime('%Y/%m/%d')
+lw = (today - timedelta(days=days_from_wed)).strftime('%Y-%m-%d')
+month = today.month
+week_of_month = (today.day - 1) // 7 
+lastweek_fri = (today - timedelta(days=days_from_fri)).strftime('%Y년 %m 월 %d일')
+
+
+
+
+_, col, _ = st.columns([2, 7, 1])
+col.title('자재센터 보고서 자동화')
+
+''
+''
+# 3개의 텝으로 분리
+tab1, tab2, tab3 = st.tabs(['주간업무일지', '회전률', '월마감'])
+
+
+# 탭1 - 주간업무일지
+with tab1:
+    st.subheader(f'{lw} ~ {td} 주간보고')
+    ''
+    st.write(f'{td} 기준 저장소품목별단가 파일 필요 ')
+    ''
+    
+    buffer = BytesIO()
+
+    df = ''
+    df2 = ''
+
+    # 파일 로드 
+    file_path = st.file_uploader('저장소품목별단가 Excel 파일을 선택하세요.')
+    out1 = st.empty()
+
+    if file_path:
+        out1.write('저장소품목별단가를 성공적으로 올렸습니다~')
+        df = pd.read_excel(file_path)
+    '---'
+    file_path2 = st.file_uploader('구매입고리스트 Excel 파일을 선택하세요.')
+    out2 = st.empty()
+
+    if file_path2:
+        out2.write('구매입고리스트를 성공적으로 올렸습니다~')
+        df2 = pd.read_excel(file_path2)
+
+    '---'
+    file_path3 = st.file_uploader('저번주데이터 Excel 파일을 선택하세요.')
+    out3 = st.empty()
+
+    if file_path3:
+        out2.write('저번주데이터를 성공적으로 올렸습니다~')
+        df_lastweek = pd.read_excel(file_path3, sheet_name='금액비교')
+
+    ''
+
+    ''
+    if file_path and file_path2 and file_path3:
+        # 데이터 처리 
+        df, df2 = process_data(df, df2)
+
+        # 금액 합계
+        summary = calculate_summary(df)
+
+        df3 = pd.DataFrame({
+            '-': list(summary.keys()),
+            '금주': list(summary.values())
+        })
+
+        if '금주' in df_lastweek.columns:
+            df3['전주'] = df_lastweek['금주'].values[:len(df3)]
+            df3['차액'] = df3['금주'] - df3['전주']
+            df3 = df3[['-', '전주', '금주', '차액']]
+
+        df3 = df3.round()
+
+        # dp 금액 증감 확인
+        hr = '증가' if df3["차액"][5] >0 else '김소'
+
+        # 주간금액 불러오기
+        wp = week_buy_price(df2, toDay, lastweek)
+
+        week_work_list ={
+            '1':f'{days}(화) 전사 재고 총 금액 {int(df3["금주"][0]):,}원 / DP {int(df3["금주"][5]):,}백 ({int(df3["차액"][5]):,}원 {hr})\n' 
+                f'- SK쉴더스 {int(df3["금주"][2]):,}백 / 교육청 {int(df3["금주"][3]):,}백 / 전자단말기 {int(df3["금주"][4]):,}백 / 스마트케어 {int(df3["금주"][1]):,}백',
+
+            '2':f'주간 구매 금액 : {wp:,}원 \n'    
+                '- DP 서비스 자재 : ECS Q670 메인보드 외 ??? 백 \n'
+                '- 교육청 자재 : 인천시교육청 판매용 마우스 외 ??? 백 \n'
+                '- 유상수리 : 기가바이트 H310M 메인보드 외 ???백',
+
+            '3':'기타\n'
+                '- 롯데렌탈 회수장비 반납 : PC, 모니터 등 총 ??EA'
+                '\n- 11월 ?주차 자재회전률 ???% (목표 50%)'
+        }
+    
+        df4 = pd.DataFrame(week_work_list.items(), columns=['구분', '내용'])
+    
+        
+        # 엑셀 저장
+        with pd.ExcelWriter(buffer, engine='openpyxl') as writer:
+            df.to_excel(writer, sheet_name='RAW', index=False)
+            df2.to_excel(writer, sheet_name='구매', index=False)
+            df3.to_excel(writer, sheet_name='금액비교', index=False)
+            df4.to_excel(writer, sheet_name='주간업무내용', index=False)
+
+        # 버퍼의 내용 가져오기
+        excel_data = buffer.getvalue()
+
+        st.download_button(label='파일 다운로드', data=excel_data, file_name=f'{to_day}raw.xlsx', mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+        st.write ('다운로드 되었습니다.')
+
+    else:
+        st.write('파일 불러와주세요.')
+
+
+
+# 탭2 - 회전률
+with tab2:
+    # 날짜 구하기
+
+    st.subheader(f'{month}월 {week_of_month}주차 회전률 RAW')
+    ''
+    st.write(f'{lastweek_fri} 파일')
+    buffer = BytesIO()
+    ''
+    # 파일 로드 
+    # 저번주 금요일 저장소 불러오기 
+    file_path = st.file_uploader('회전률 저장소품목별단가 Excel 파일을 선택하세요.')
+    out1 = st.empty()
+
+    if file_path:
+        out1.write('회전률 저장소품목별단가를 성공적으로 올렸습니다~')
+        df = pd.read_excel(file_path)
+    '---'
+    file_path2 = st.file_uploader('S/L 간 이동처리 리스트 Excel 파일을 선택하세요.')
+    out2 = st.empty()
+
+    if file_path2:
+        out2.write('S/L 간 이동처리 리스트를 성공적으로 올렸습니다~')
+        df2 = pd.read_excel(file_path2)
+
+    ''
+    ''
+    if file_path and file_path2:
+        df, df2 = data_process(df, df2)
+
+        with pd.ExcelWriter(buffer, engine='openpyxl') as writer:
+                df.to_excel(writer, sheet_name='재고', index=False)
+                df2.to_excel(writer, sheet_name='반납금액', index=False)
+
+        # 버퍼의 내용 가져오기
+        excel_data = buffer.getvalue()
+
+        st.download_button(label='파일 내려 받기', data=excel_data, file_name='회전률raw.xlsx', mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+        st.write ('다운로드 되었습니다.')
+
+    else:
+        st.write('파일을 불러와주세요.')
---
+++ 보고서_함수.py
@@ -0,0 +1,129 @@
+# 주간업무일지 함수 1
+def process_data(df, df2):
+    """데이터 처리 함수"""
+    # 첫 행(단가 합계) 삭제 
+    df = df.drop(index=0)
+    df2 = df2.drop(index=0)
+
+    # 저장소품목별단가 정리
+    # 단가열 삭제
+    df = df.drop(['단가'], axis=1)
+
+    # 필터링 
+    filters = {
+        '품목번호' : r'^[AMXE]',
+        '창고' : r'렌탈'
+    }
+
+    for column, pattern in filters.items():
+        df = df[~df[column].str.contains(pattern, regex=True, na=False)]
+
+    # 구매입고 리스트 정리
+    # 원하는 열만 가지고 오기
+    df2 = df2[['입고일자', '거래처명', '품목', '품목명', '규격', '입고수량', '원화단가', '원화금액']] 
+    df2 = df2.rename(columns={'품목':'품목번호'})
+
+    # 재고단가 0 인 행 업데이트 하기 
+    #df.loc[df['재고단가'] == 0, '재고단가'] = df.loc[df['재고단가'] == 0, '품목번호'].map(df2.set_index('품목번호')['원화단가'])
+    # 중복코드값이 있어 에러 발생 
+
+    # 중복코드값이 있어도 실행 - 공부할것!!! 
+    mask = df['재고단가'] == 0
+    df_temp = df[mask][['품목번호']].copy()
+
+    # df2와 merge (중복이 있어도 작동)
+    df_temp = df_temp.merge(df2[['품목번호', '원화단가']], on='품목번호', how='left')
+
+    # 원래 데이터프레임에 업데이트
+    df.loc[mask, '재고단가'] = df_temp['원화단가'].values
+    df['금액'] = df['재고단가'] * df['창고재고']
+
+    return df, df2
+
+# 주간업무일지 함수 2
+def calculate_summary(df):
+    """창고별 금액 합계""" 
+    summary = {
+        '총액': df['금액'].sum(),
+        '마을방송': df[df['품목번호'].str.startswith('B', na=False)]['금액'].sum(),
+        '쉴더스': df[df['규격'].str.contains('리퍼', na=False)]['금액'].sum(),
+        '교육청': df[df['규격'].str.contains('교육청', na=False)]['금액'].sum(),
+        '전자단말': df[df['품목번호'].str.startswith('C', na=False)]['금액'].sum(),
+    }
+    summary['dp'] = summary['총액'] - sum([summary[k] for k in ['마을방송', '쉴더스', '교육청', '전자단말']])    
+
+    return summary
+
+# 주간업무일지 함수 3
+def week_buy_price(df2, toDay, lastweek):
+    """주간 구매 금액 합계"""
+    week_price = df2[(df2['입고일자'] < toDay) & (df2['입고일자'] >= lastweek)]['원화금액'].sum()
+
+    return week_price
+
+
+
+
+
+# 회전률 함수 1
+def data_process(df, df2):
+    """ 회전률 데이터 처리 함수"""
+    # 첫 행 삭제
+    df = df.drop(index=0)
+
+    # 조건 정의 
+    delete_conditions = [
+        ('품목번호', 'startswith', 'X'),
+        ('품목번호', 'startswith', 'B'),
+        ('창고', 'contains', '영업'),
+        ('창고', 'contains', '전자'),
+        ('창고', 'contains', '자재'),
+        ('창고', 'contains', '상주'),
+        ('창고', 'contains', '인천'),
+        ('규격', 'contains', 'AFWM'),
+        ('규격', 'contains', 'APD3'),
+        ('규격', 'contains', '교육청'),
+        ('규격', 'contains', '전자칠판'),
+        ('규격', 'contains', '건강보험'),
+        ('규격', 'contains', '연금공단'),
+    ]
+
+    # 반복문으로 처리
+    for column, method, value in delete_conditions:
+        if method == 'startswith':
+            df = df[~df[column].str.startswith(value)]
+        elif method == 'contains':
+            df = df[~df[column].str.contains(value)]
+
+    # 
+    df2 = df2[['이동일자', '품목', '품목명', '규격', '단위', '이동수량', '출고창고', '입고창고', '담당부서', '담당자']]
+
+
+    delete_conditions2 = [
+        ('품목', 'startswith', 'X'),
+        ('입고창고', 'contains', '자재'),
+        ('입고창고', 'contains', '영업'),
+        ('출고창고', 'contains', '자재'),
+        ('규격', 'contains', 'AFWM'),
+        ('규격', 'contains', 'APD3'),
+        ('규격', 'contains', '교육청'),
+        ('규격', 'contains', '전자칠판'),
+        ('규격', 'contains', '건강보험'),
+        ('규격', 'contains', '연금공단'),
+    ]
+
+    for column, method, value in delete_conditions2:
+        if method == 'startswith':
+            df2 = df2[~df2[column].str.startswith(value)]
+        elif  column == '입고창고' and value == '자재':
+            df2 = df2[df2[column].str.contains(value)]
+        else:
+            df2 = df2[~df2[column].str.contains(value)]
+
+    return df, df2
+
+
+
+
+
+
